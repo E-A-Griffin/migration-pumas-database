10568ee2d78e7d3809ff275ec0e5644d7b6d5290
E-A-Griffin <eagriffin@usf.edu>
2022-04-30 17:12:37 -0400

    Add documentation for Clojure preprocessing script

--- py_points_to_postgres.clj
+++ py_points_to_postgres.clj
@@ -1,18 +1,29 @@
+;; Convert python point syntax to postgres point syntax
 (ns python->postgres
   (:require [clojure.string :as str]))
 
 
 (defn split-on-comma [s] (str/split s #","))
 
-(defn is-point? [s] (or (re-matches
-                         #"\([+-]?([0-9]*[.])?[0-9]+ [+-]?([0-9]*[.])?[0-9]+\)"
-                         s)
-                        (re-matches
-                         #" \([+-]?([0-9]*[.])?[0-9]+ [+-]?([0-9]*[.])?[0-9]+\)"
-                         s)))
+(defn is-point?
+  "True iff s is a Python Point"
+  [s] (or (re-matches
+           #"\([+-]?([0-9]*[.])?[0-9]+ [+-]?([0-9]*[.])?[0-9]+\)"
+           s)
+          (re-matches
+           #" \([+-]?([0-9]*[.])?[0-9]+ [+-]?([0-9]*[.])?[0-9]+\)"
+           s)))
 
-(defn py-pnt->postgres [s] (str/replace (str/trim s) #" " ", "))
+(defn py-pnt->postgres
+  "Takes a string representing a Python Point and returns a string representing
+   an equivalent PostgreSQL Point"
+  [s] (str/replace (str/trim s) #" " ", "))
 
-(defn py-pnts->postgres [s] (str/join "\n" (map (partial str/join ", ") (map (comp (partial map #(if (is-point? %)
-                                                                 (py-pnt->postgres %) %))
-                                                 split-on-comma) (str/split-lines s)))))
+(defn py-pnts->postgres
+  "Takes a string representing n Python Points and returns a string
+   representing n equivalent PostgreSQL Points"
+  [s]
+  (str/join "\n" (map (partial str/join ", ")
+                      (map (comp (partial map #(if (is-point? %)
+                                                 (py-pnt->postgres %) %))
+                                 split-on-comma) (str/split-lines s)))))
